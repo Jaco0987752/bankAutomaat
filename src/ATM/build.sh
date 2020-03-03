250c0fa65153ae5e2e5212fde2ede0a9f9a45b83
Jaco0987752 <0987752@hr.nl>
2020-03-03 10:45:43 +0100

    making debugging possible

--- src/ATM/build.sh
+++ src/ATM/build.sh
@@ -2,7 +2,7 @@
 CWD=$(PWD)
 JAVA_FX=lib/javafx-sdk-13.0.2/lib
 REALPATH_JAVA_FX=$CWD/$JAVA_FX
-
+CLASSPATH=lib/gson-2.8.6.jar
 echo $REALPATH_JAVA_FX
 
 echo $CWD
@@ -13,14 +13,17 @@ DIR=bin
  fi
 
 #-d $DIR
- if javac -d $DIR --module-path $REALPATH_JAVA_FX --add-modules javafx.controls,javafx.fxml $(find . -name '*.java');
+ if javac -d $DIR --module-path $REALPATH_JAVA_FX -cp $CLASSPATH --add-modules javafx.controls,javafx.fxml $(find . -name '*.java');
 then
     echo classes compiled
 
-    cp -r ./View ./$DIR/ATM
+    cp -r ./View ./$DIR
     cd ./$DIR
-    jar cfe App.jar ATM.App ./ATM/
+    jar cfe App.jar ViewModel.App ./
     echo jar made
 
     java  --module-path $REALPATH_JAVA_FX --add-modules javafx.controls,javafx.fxml -jar App.jar
 fi
+//cd ../
+rm  -r ./Model ./View ./ViewModel
+rm -r $(find . -name '*.class') $(find . -name '*.fxml')
