cd35dcd1ab98d79999054e1bb8ca394d84bcf56c
Jaco0987752 <0987752@hr.nl>
2020-03-12 12:19:50 +0100

    Removing unused code

--- src/ATM/Model/CardChecker.java
+++ src/ATM/Model/CardChecker.java
@@ -5,39 +5,5 @@ package Model;
  */
 public class CardChecker {//implements Runnable {
 
-    // private NotifyRFIDCardAccepted notifyCardAccepted;
-    // private Thread thread;
 
-    // public RFIDLogic() {
-    //     startThread();
-    // }
-
-    // private void startThread() {
-    //     if (thread == null) {
-    //         thread = new Thread(this, "RFID thread");
-    //         thread.start();
-    //     }
-    // }
-
-    // private void ThreadWait(long millies) {
-    //     try{
-    //         Thread.sleep(millies);
-    //     }catch (Exception e){
-    //         System.out.println("Thread interrupted");
-    //     }
-    // }
-    
-    // public void SetNotifyCardAccepted(NotifyRFIDCardAccepted notifyCardAccepted){
-    //     this.notifyCardAccepted = notifyCardAccepted;
-    // }
-
-    // @Override
-    // public void run() {
-    //     ThreadWait(10000);
-    //     if (notifyCardAccepted != null) {
-    //         notifyCardAccepted.RFIDCardAccepted();
-    //     }
-    // }
-
-    // public void CheckCard(){}
 }
