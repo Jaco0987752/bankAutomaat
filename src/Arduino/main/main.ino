41645365e3c553c2d13cea0841be641e73c53897
Jaco0987752 <0987752@hr.nl>
2020-03-13 10:52:36 +0100

    Adding hello world

--- src/Arduino/main/main.ino
+++ src/Arduino/main/main.ino
@@ -1,7 +1,13 @@
-void setup(void){
+#include <Arduino.h>
 
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("hello world");
 }
 
-void loop(void){
-    
+void loop() {
+  delay(1000);
+  Serial.println("s");
+
 }
